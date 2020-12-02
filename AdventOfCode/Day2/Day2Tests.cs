04344afc78a81247c1f36a001bbd9164a6b4a9d1
Meriem Chelbi <Meriem.Chelbi@thetrainline.com>
2020-12-02 20:46:11 +0000

    Day 2 part 2

--- AdventOfCode/Day2/Day2Tests.cs
+++ AdventOfCode/Day2/Day2Tests.cs
@@ -24,14 +24,38 @@ namespace AdventOfCode.Day2
             Assert.Equal(546, result);
         }
 
-        //[Fact]
-        //public void Part2_Result()
-        //{
-        //    Solver solver = new();
+        [Theory]
+        [InlineData("abcde", 'a', 1, 3, true)]
+        [InlineData("cdefg", 'b', 1, 3, false)]
+        [InlineData("ccccccccc", 'c', 2, 9, false)]
+        [InlineData("jjjjjjjjjjjjjjjj", 'j', 13, 15, false)]
+        [InlineData("jmjdbhjjjjjjjxnjj", 'j', 2, 14, false)]
+        [InlineData("qdqqkqtbcnqqqsrklqbf", 'q', 3, 9, true)]
+        public void ValidateLookup(string password, char letter, int min, int max, bool expected)
+        {
+            Solver solver = new();
+            var sample = new PasswordSample
+            {
+                Password = password,
+                Letter = letter,
+                Min = min,
+                Max = max
+            };
 
-        //    var result = solver.Solve3();
+            var result = solver.ValidateLookup(sample);
 
-        //    Assert.Equal(200878544, result);
-        //}
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_Result()
+        {
+            Solver solver = new();
+
+            var result = solver.Solve2();
+
+            Assert.True(result > 138);
+            Assert.True(result < 725);
+        }
     }
 }
--- AdventOfCode/Day2/Solver.cs
+++ AdventOfCode/Day2/Solver.cs
@@ -12,19 +12,41 @@ namespace AdventOfCode.Day2
 
             foreach (var password in input)
             {
-                if (Validate(password))
+                if (ValidateCounting(password))
                     password.IsValid = true;
             }
 
             return input.Count(i => i.IsValid);
         }
 
-        private bool Validate(PasswordSample password)
+        private bool ValidateCounting(PasswordSample password)
         {
             int letterCount = password.Password.Count(l => l.Equals(password.Letter));
             
             return letterCount >= password.Min 
                    && letterCount <= password.Max;
+        }
+        
+        internal int Solve2()
+        {
+            var parser = new InputParser();
+            var input = parser.Parse();
+
+            foreach (var password in input)
+            {
+                if (ValidateLookup(password))
+                    password.IsValid = true;
+            }
+
+            return input.Count(i => i.IsValid);
+        }
+
+        internal bool ValidateLookup(PasswordSample password)
+        {
+            var minPosContains = password.Password[password.Min - 1] == password.Letter;
+            var maxPosContains = password.Password[password.Max - 1] == password.Letter;
+            return (minPosContains && !maxPosContains)
+                || (!minPosContains && maxPosContains);
         }
     }
 }
