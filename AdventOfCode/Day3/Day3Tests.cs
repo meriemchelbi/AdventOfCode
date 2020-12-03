cbe5a9775a2c01382fd332c6be649ed9df35882a
Meriem Chelbi <Meriem.Chelbi@thetrainline.com>
2020-12-03 13:00:22 +0000

    Day 3 part 2

--- AdventOfCode/Day3/Day3Tests.cs
+++ AdventOfCode/Day3/Day3Tests.cs
@@ -15,14 +15,18 @@ namespace AdventOfCode.Day3
         }
 
         [Theory]
-        [InlineData(30, 25, 28)]
-        [InlineData(30, 28, 1)]
-        [InlineData(30, 29, 2)]
-        public void CalculatePosition(int rowLength, int currentPosition, int expectedPosition)
+        [InlineData(30, 25, 3, 28)]
+        [InlineData(30, 28, 3, 1)]
+        [InlineData(30, 29, 3, 2)]
+        [InlineData(31, 29, 1, 30)]
+        [InlineData(31, 30, 1, 0)]
+        [InlineData(31, 29, 5, 3)]
+        [InlineData(31, 28, 7, 4)]
+        public void CalculatePosition(int rowLength, int currentPosition, int rightSlope, int expectedPosition)
         {
             Solver solver = new();
 
-            var result = solver.CalculatePosition(currentPosition, rowLength, 3);
+            var result = solver.CalculatePosition(currentPosition, rowLength, rightSlope);
 
             Assert.Equal(expectedPosition, result);
         }
@@ -44,8 +48,7 @@ namespace AdventOfCode.Day3
 
             var result = solver.Solve2();
 
-            Assert.True(result > 138);
-            Assert.True(result < 725);
+            Assert.Equal(2698900776, result);
         }
     }
 }
--- AdventOfCode/Day3/Solver.cs
+++ AdventOfCode/Day3/Solver.cs
@@ -13,7 +13,7 @@ namespace AdventOfCode.Day3
             var rowPos = 0;
             var rowLength = input[0].Length;
 
-            for (int i = 1; i < input.Count; i += downSlope)
+            for (int i = downSlope; i < input.Count; i += downSlope)
             {
                 rowPos = CalculatePosition(rowPos, rowLength, rightSlope);
 
@@ -40,7 +40,7 @@ namespace AdventOfCode.Day3
         public int CalculatePosition(int rowPos, int rowLength, int rightSlope)
         {
             var maxIndex = rowLength - 1;
-            if (rowPos <= maxIndex - 3)
+            if (rowPos <= maxIndex - rightSlope)
                 rowPos += rightSlope;
             else
             {
@@ -51,9 +51,21 @@ namespace AdventOfCode.Day3
             return rowPos;
         }
 
-        internal int Solve2()
+        internal uint Solve2()
         {
-            return 0;
+            var slope1 = (uint)Solve(1, 1);
+            var slope2 = (uint)Solve(3, 1);
+            var slope3 = (uint)Solve(5, 1);
+            var slope4 = (uint)Solve(7, 1);
+            var slope5 = (uint)Solve(1, 2);
+
+            //var a = slope1 * slope2;
+            //var b = a * slope3;
+            //var c = b * slope4;
+            //var d = c * slope5;
+            var product = slope1 * slope2 * slope3 * slope4 * slope5;
+
+            return product;
         }
     }
 }
