a4d267724d0085ac9de7a8324be38f4d6129bdef
sprogslegs <meriemchelbi@outlook.com>
2020-12-04 00:37:22 +0000

    Day 4 skeleton

---
+++ AdventOfCode/Day4/Day4Tests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace AdventOfCode.Day4
+{
+    public class Day4Tests
+    {
+        [Fact]
+        public void InputParser_ReturnsList()
+        {
+            InputParser parser = new();
+
+            var result = parser.Parse();
+
+            Assert.Equal(323, result.Count);
+        }
+
+        [Fact]
+        public void Part1_Result()
+        {
+            Solver solver = new();
+
+            var result = solver.Solve(3, 1);
+
+            Assert.Equal(262, result);
+        }
+
+        [Fact]
+        public void Part2_Result()
+        {
+            Solver solver = new();
+
+            var result = solver.Solve2();
+
+            Assert.Equal(2698900776, result);
+        }
+    }
+}
---
+++ AdventOfCode/Day4/Solver.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode.Day4
+{
+    class Solver
+    {
+        internal int Solve(int rightSlope, int downSlope)
+        {
+            return 0;
+        }
+
+        internal uint Solve2()
+        {
+            return 0;
+        }
+    }
+}
