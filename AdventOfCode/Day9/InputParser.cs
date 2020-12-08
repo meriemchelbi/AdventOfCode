8d4813d61cce7653ecbe279cfe03656e300d97f2
sprogslegs <meriemchelbi@outlook.com>
2020-12-08 08:19:35 +0000

    Day 9 skeleton

---
+++ AdventOfCode/Day9/InputParser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode.Day9
+{
+    class InputParser
+    {
+        public List<string> Parse()
+        {
+            var output = new List<string>();
+            var path = Path.GetFullPath("Day9\\Input.txt");
+
+            using (var sr = new StreamReader(path))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    
+                }
+            }
+
+            return output;
+        }
+    }
+}
---
+++ AdventOfCode/Day9/Solver.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode.Day9
+{
+    class Solver
+    {
+        internal int Solve()
+        {
+            var parser = new InputParser();
+            var input = parser.Parse();
+
+            return 0;
+        }
+
+        internal int Solve2()
+        {
+            var parser = new InputParser();
+            var input = parser.Parse();
+
+            return 0;
+        }
+    }
+}
