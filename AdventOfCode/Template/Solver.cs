501e2c9d7a31b2d3ffac223a365bf69adfe2c218
sprogslegs <meriemchelbi@outlook.com>
2021-01-02 16:58:01 +0000

    Template skeleton

---
+++ AdventOfCode/Template/Solver.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode.Template
+{
+    class Solver
+    {
+        internal int Solve(int rightSlope, int downSlope)
+        {
+            InputParser parser = new();
+            var input = parser.Parse();
+
+            return 0;
+        }
+
+        internal uint Solve2()
+        {
+            InputParser parser = new();
+            var input = parser.Parse();
+
+            return 0;
+        }
+    }
+}
