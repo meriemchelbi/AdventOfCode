fb49b20144cd12f9f984eb3fc1369169176b4122
sprogslegs <meriemchelbi@outlook.com>
2021-01-02 16:54:08 +0000

    Day 13 part 2 (WIP)

--- AdventOfCode/Day13/Day13Tests.cs
+++ AdventOfCode/Day13/Day13Tests.cs
@@ -27,14 +27,31 @@ namespace AdventOfCode.Day13
             Assert.True(result < 4568);
         }
 
+        [Theory]
+        [InlineData(true, 3, 3417, "17", "x", "13", "19")]
+        [InlineData(true, 4, 754018, "67","7","59","61")]
+        [InlineData(true, 4, 779210, "67", "x", "7","59","61")]
+        [InlineData(true, 4, 1261476, "67", "7", "x", "59","61")]
+        [InlineData(true, 4, 1202161486, "1789","37","47","1889")]
+        public void IsWinningTimestamp(bool expected, int busCount, uint timestamp, params string[] timetable)
+        {
+            Solver solver = new();
+
+            var busIds = solver.ExtractIndexedBusIds(timetable);
+
+            var result = solver.IsWinningTimestamp(busIds, timestamp);
+
+            Assert.Equal(expected, result);
+        }
+
         [Fact]
         public void Part2_Result()
         {
             Solver solver = new();
 
-            var result = solver.Solve2("Input");
+            var result = solver.Solve3("Input");
 
-            Assert.NotEqual(46639, result);
+            //Assert.NotEqual(46639, result);
         }
     }
 }
--- AdventOfCode/Day13/Solver.cs
+++ AdventOfCode/Day13/Solver.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Day13
@@ -34,12 +35,110 @@ namespace AdventOfCode.Day13
             return earliestBus * minutesWait;
         }
 
-        internal int Solve2(string inputFileName)
+        internal UInt64 Solve2(string inputFileName)
         {
             var parser = new InputParser();
             var input = parser.Parse(inputFileName);
-            
-            return 0;
+
+            var indexedBusIds = ExtractIndexedBusIds(input.Timetable);
+            var busIds = input.Timetable.Where(t => !t.Equals("x"))
+                                        .Select(b => int.Parse(b)).ToArray();
+            var firstBus = (UInt64) indexedBusIds[0];
+
+            UInt64 winningTimestamp = 0;
+
+            for (UInt64 i = firstBus; i < UInt64.MaxValue; i += (UInt64)busIds[0])
+            {
+                var isWinner = IsWinningTimestamp(indexedBusIds, i);
+
+                if (isWinner)
+                {
+                    winningTimestamp = i;
+                    break;
+                }
+            }
+
+            return winningTimestamp;
+        }
+
+        internal Dictionary<int, int> ExtractIndexedBusIds(string[] timetable)
+        {
+            var result = new Dictionary<int, int>();
+
+            for (int i = 0; i < timetable.Length; i++)
+            {
+                if (timetable[i].Equals("x"))
+                    continue;
+
+                else
+                    result[i] = int.Parse(timetable[i]);
+            }
+
+            return result;
+        }
+        
+        internal Dictionary<int, int> ExtractRelativeBusIds(string[] timetable, int rootBusIndex)
+        {
+            var result = new Dictionary<int, int>();
+            var list = timetable.ToList();
+            var rootBusId = int.Parse(timetable[rootBusIndex]);
+
+            result[0] = rootBusId;
+
+            for (int i = 0; i < timetable.Length; i++)
+            {
+                if (timetable[i].Equals("x"))
+                    continue;
+
+                else
+                {
+                    var index = i - rootBusIndex;
+                    result[index] = int.Parse(timetable[i]);
+                }
+            }
+
+            return result;
+        }
+
+        internal bool IsWinningTimestamp(Dictionary<int, int> indexedBusIds, UInt64 timestamp)
+        {
+            foreach (var bus in indexedBusIds)
+            {
+                var busId = bus.Value;
+                var nextTimestamp = timestamp + (ulong) bus.Key;
+                var hasRemainder = (nextTimestamp % (ulong) busId) != 0;
+
+                if (hasRemainder)
+                    return false;
+            }
+
+            return true;
+        }
+
+        internal ulong Solve3(string inputFileName)
+        {
+            var parser = new InputParser();
+            var input = parser.Parse(inputFileName);
+            var busIds = input.Timetable.Where(t => !t.Equals("x"))
+                                        .Select(b => int.Parse(b)).ToList();
+            var highestBusId = busIds.Max();
+            var highestBusIndex = input.Timetable.ToList().IndexOf(highestBusId.ToString());
+            var indexedBusIds = ExtractRelativeBusIds(input.Timetable, highestBusIndex);
+
+            UInt64 winningTimestamp = 0;
+
+            for (ulong i = 100000000000000; i < ulong.MaxValue; i += (ulong)highestBusId)
+            {
+                var isWinner = IsWinningTimestamp(indexedBusIds, i);
+
+                if (isWinner)
+                {
+                    winningTimestamp = i;
+                    break;
+                }
+            }
+
+            return winningTimestamp;
         }
 
         private List<int> ExtractBusIds(string[] timetable)
