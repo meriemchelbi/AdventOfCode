9ded55932b71b35bcf1b7466915ae470cdbbc642
sprogslegs <47434119+sprogslegs@users.noreply.github.com>
2021-12-02 09:40:57 +0000

    Day 3 setup

---
+++ AdventOfCode/Day3/D3Tests.cs
@@ -0,0 +1,43 @@
+﻿using AdventOfCode.InputParsers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Day3
+{
+    public class D3Tests
+    {
+        private readonly ToTupleParser _parser;
+        private readonly Solver _sut;
+
+        public D3Tests()
+        {
+            _sut = new Solver();
+        }
+
+        [Fact]
+        public void Part1TestCase()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Part2TestCase()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void SolvePart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode/Day3/Solver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Day3
+{
+    public class Solver
+    {
+        public int SolvePart1(List<(char, int)> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SolvePart2(List<(char, int)> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
