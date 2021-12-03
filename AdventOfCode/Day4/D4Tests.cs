b51c3196917481582b408117c3ead8e71c47b222
sprogslegs <47434119+sprogslegs@users.noreply.github.com>
2021-12-03 18:10:29 +0000

    Day 4 setup

---
+++ AdventOfCode/Day4/D4Tests.cs
@@ -0,0 +1,42 @@
+﻿using AdventOfCode.InputParsers;
+using System;
+using Xunit;
+
+namespace AdventOfCode.Day4
+{
+    public class D4Tests
+    {
+        private readonly ToListParser _parser;
+        private readonly Solver _sut;
+
+        public D4Tests()
+        {
+            _parser = new ToListParser();
+            _sut = new Solver();
+        }
+
+        [Fact]
+        public void Part1TestCase()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Part2TestCase()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void SolvePart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
