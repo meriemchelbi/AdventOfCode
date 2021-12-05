26138762b134cc151bd0c0757ad5a25c9c5db674
sprogslegs <47434119+sprogslegs@users.noreply.github.com>
2021-12-05 08:54:37 +0000

    Day 5 setup

---
+++ AdventOfCode/Day5/D5Tests.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode.InputParsers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Day5
+{
+    public class D5Tests
+    {
+        private readonly ToListParser _parser;
+        private readonly Solver _sut;
+
+        public D5Tests()
+        {
+            _parser = new ToListParser();
+            _sut = new Solver();
+        }
+
+        [Fact]
+        public void Part1TestCase()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Part2TestCase()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void SolvePart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode/Day5/Solver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Day5
+{
+    public class Solver
+    {
+        public int SolvePart1(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SolvePart2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
