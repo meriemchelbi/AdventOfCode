5e2275703481bcc897e84fcf431855b353ed1486
sprogslegs <47434119+sprogslegs@users.noreply.github.com>
2021-12-05 16:15:16 +0000

    Day 6 prep

---
+++ AdventOfCode/Day6/D6Tests.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode.InputParsers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Day6
+{
+    public class D6Tests
+    {
+        private readonly ToListParser _parser;
+        private readonly Solver _sut;
+
+        public D6Tests()
+        {
+            _parser = new ToListParser();
+            _sut = new Solver();
+        }
+
+        [Fact]
+        public void Part1TestCase()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Part2TestCase()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void SolvePart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
