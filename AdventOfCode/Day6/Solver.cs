96195eb725201f5286beecd7d51fa4b05f071c3a
Meriem Chelbi <47434119+sprogslegs@users.noreply.github.com>
2021-12-06 17:49:55 +0000

    Day 6 WIP

--- AdventOfCode/Day6/Solver.cs
+++ AdventOfCode/Day6/Solver.cs
@@ -7,9 +7,50 @@ namespace AdventOfCode.Day6
 {
     public class Solver
     {
-        public int SolvePart1(List<string> input)
+        public int SolvePart1(List<int> input)
         {
-            throw new NotImplementedException();
+            var lifespanCount = new Dictionary<int, int> 
+            { 
+                { 0, 0 },
+                { 1, 0 },
+                { 2, 0 },
+                { 3, 0 },
+                { 4, 0 },
+                { 5, 0 },
+                { 6, 0 },
+                { 7, 0 },
+                { 8, 0 }
+            };
+
+            foreach (var fishLifespan in input)
+            {
+                lifespanCount[fishLifespan] += 1;
+            }
+
+            for (int i = 0; i < 80; i++)
+            {
+                for (int lifespan = 0; lifespan < lifespanCount.Count; lifespan++)
+                {
+                    int countOfLifespan = lifespanCount[lifespan];
+                    
+                    if (lifespan == 0)
+                    {
+
+                        lifespanCount[8] += countOfLifespan;
+                        lifespanCount[6] += countOfLifespan;
+                        lifespanCount[0] = 0;
+                    }
+
+                    else
+                    {
+                        var newLifespan = lifespan - 1;
+                        lifespanCount[newLifespan] += countOfLifespan;
+                        lifespanCount[lifespan] = 0;
+                    }
+                }
+            }
+
+            return lifespanCount.Select(l => l.Key * l.Value).Sum();
         }
 
         public int SolvePart2(List<string> input)
