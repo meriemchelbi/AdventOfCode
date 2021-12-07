b10d56f451739f43538ec8b67137186fce6e213a
sprogslegs <47434119+sprogslegs@users.noreply.github.com>
2021-12-07 08:25:38 +0000

    Day 7 setup

---
+++ AdventOfCode/Day7/D7Tests.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode.InputParsers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Day7
+{
+    public class D7Tests
+    {
+        private readonly ToListParser _parser;
+        private readonly Solver _sut;
+
+        public D7Tests()
+        {
+            _parser = new ToListParser();
+            _sut = new Solver();
+        }
+
+        [Fact]
+        public void Part1TestCase()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Part2TestCase()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void SolvePart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode/Day7/Solver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Day7
+{
+    public class Solver
+    {
+        public int SolvePart1(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SolvePart2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
