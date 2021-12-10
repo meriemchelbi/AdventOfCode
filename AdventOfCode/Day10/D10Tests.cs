c456e6f01d8e0c588861012a22f3bc559cd9e5af
Meriem Chelbi <meriem.chelbi@thetrainline.com>
2021-12-10 17:04:46 +0000

    Day 10 setup

---
+++ AdventOfCode/Day10/D10Tests.cs
@@ -0,0 +1,60 @@
+﻿using AdventOfCode.InputParsers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Day10
+{
+    public class D10Tests
+    {
+        private readonly ToListParser _parser;
+        private readonly Solver _sut;
+
+        public D10Tests()
+        {
+            _parser = new ToListParser();
+            _sut = new Solver();
+        }
+
+        [Fact]
+        public void Part1TestCase()
+        {
+            var input = _parser.ParseToListOfString("Day10\\TestInput.txt");
+
+            var result = _sut.SolvePart1(input);
+
+            result.Should().Be(26397);
+        }
+
+        [Fact]
+        public void Part2TestCase()
+        {
+            var input = _parser.ParseToListOfString("Day10\\TestInput.txt");
+
+            var result = _sut.SolvePart2(input);
+
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void SolvePart1()
+        {
+            var input = _parser.ParseToListOfString("Day10\\Input.txt");
+
+            var result = _sut.SolvePart1(input);
+
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void SolvePart2()
+        {
+            var input = _parser.ParseToListOfString("Day10\\Input.txt");
+
+            var result = _sut.SolvePart2(input);
+
+            result.Should().Be(0);
+        }
+    }
+}
