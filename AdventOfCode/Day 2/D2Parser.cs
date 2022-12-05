52c3f578ecdc50772a027e01c70e68e0db277be6
Meriem Chelbi <meriem.chelbi@thetrainline.com>
2022-12-05 12:58:45 +0000

    Day 2 (WIP)

---
+++ AdventOfCode/Day 2/D2Parser.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day_2
+{
+    public class D2Parser
+    {
+        public List<(int, int)> Parse(string inputPath)
+        {
+            var output = new List<(int, int)>();
+
+            var absolutePath = Path.GetFullPath(inputPath);
+
+            using (var sr = new StreamReader(absolutePath))
+            {
+                string line;
+                
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var strategy = line.Split(' ');
+                    var intMoves = new List<int>();
+
+                    foreach (var move in strategy)
+                    {
+                        intMoves.Add(MatchMove(move));
+                    }
+                    
+                    //var intMoves = strategy.Select(MatchMove);
+
+                    // Validation
+                    if (intMoves.Any(m => m > 3))
+                    {
+                        throw new Exception("bad data found!");
+                    }
+
+                    var moves = (intMoves[0] , intMoves[1]);
+                    
+                    output.Add(moves);
+                }
+            }
+
+            return output;
+        }
+
+        private int MatchMove(string move)
+        {
+            switch (move)
+            {
+                // Rock
+                case "A":
+                case "X": 
+                    return 1;
+                
+                // Paper
+                case "B":
+                case "Y": 
+                    return 2;
+                
+                // Scissors
+                case "C":
+                case "Z": 
+                    return 3;
+                
+                default:
+                    return 1000000;
+            }
+        }
+    }
+}
---
+++ AdventOfCode/Day 2/D2Tests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AdventOfCode.Day_2
+{
+    public class D2Tests
+    {
+        private readonly D2Parser _parser;
+        private readonly D2Solver _solver;
+
+        public D2Tests()
+        {
+            _parser = new D2Parser();
+            _solver = new D2Solver();
+        }
+
+        [Fact]
+        public void TestInput_Parses()
+        {
+            var expected = new List<(int, int)> { (1, 2), (2, 1), (3, 3) };
+
+            var path = "Day 2\\D2TestInput.txt";
+            var parsed = _parser.Parse(path);
+
+            Assert.Equal(expected, parsed);
+        }
+
+        [Fact]
+        public void Part1_Test()
+        {
+            var expected = 15;
+
+            var path = "Day 2\\D2TestInput.txt";
+            var data = _parser.Parse(path);
+
+            var result = _solver.SolvePart1(data);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part1_Actual()
+        {
+            // too low
+            var expected = 12940;
+
+            var path = "Day 2\\D2Input.txt";
+            var data = _parser.Parse(path);
+
+            var result = _solver.SolvePart1(data);
+
+            Assert.Equal(expected, result);
+        }
+
+        //[Fact]
+        //public void Part1_MostCalories_Actual()
+        //{
+        //    var expected = 69693;
+
+        //    var path = "Day 1\\D1Input.txt";
+        //    var data = _parser.Parse(path);
+        //    var mostCalories = _solver.SolvePart1(data);
+
+        //    Assert.Equal(expected, mostCalories);
+        //}
+
+        //[Fact]
+        //public void Part2_Top3_Test()
+        //{
+        //    var expected = 45000;
+
+        //    var path = "Day 1\\D1TestInput.txt";
+        //    var data = _parser.Parse(path);
+        //    var mostCalories = _solver.SolvePart2(data);
+
+        //    Assert.Equal(expected, mostCalories);
+        //}
+
+        //[Fact]
+        //public void Part2_Top3_Actual()
+        //{
+        //    var expected = 200945;
+
+        //    var path = "Day 1\\D1Input.txt";
+        //    var data = _parser.Parse(path);
+        //    var mostCalories = _solver.SolvePart2(data);
+
+        //    Assert.Equal(expected, mostCalories);
+        //}
+    }
+}
