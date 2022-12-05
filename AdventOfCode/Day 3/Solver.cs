b098954508a00a622ac2cc26ac41be1832d59278
Meriem Chelbi <meriem.chelbi@thetrainline.com>
2022-12-05 13:02:25 +0000

    Day 3 setup

---
+++ AdventOfCode/Day 3/Solver.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Day_3
+{
+    public class Solver
+    {
+        public int SolvePart1(List<(int, int)> strategy)
+        {
+            var totalScore = 0;
+
+            foreach (var round in strategy)
+            {
+                var score = RoundOutcome(round) + round.Item2;
+                
+                // Validation
+                if (score > 9)
+                {
+                    throw new Exception("invalid score!");
+                }
+
+                totalScore += score;
+            }
+            
+            return totalScore;
+        }
+        
+        //public int SolvePart2(List<int> totalCaloriesPerElf)
+        //{
+        //    var orderedDescending = totalCaloriesPerElf.OrderByDescending(e => e);
+        //    var topThree = orderedDescending.Take(3);
+
+        //    var total = 0;
+
+        //    foreach (var item in topThree)
+        //    {
+        //        total += item;
+        //    }
+            
+        //    return total;
+        //}
+
+        private int RoundOutcome((int, int) strategy)
+        {
+            // draw
+            if (strategy.Item1 == strategy.Item2)
+            {
+                return 3;
+            }
+            
+            // win
+            if (strategy.Item1 < strategy.Item2)
+            {
+                return 6;
+            }
+
+            // loss
+            if (strategy.Item1 > strategy.Item2)
+            {
+                return 0;
+            }
+
+            else return 1000;
+        }
+    }
+}
---
+++ AdventOfCode/Day 3/Tests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AdventOfCode.Day_3
+{
+    public class Tests
+    {
+        private readonly Parser _parser;
+        private readonly Solver _solver;
+
+        public Tests()
+        {
+            _parser = new Parser();
+            _solver = new Solver();
+        }
+
+        [Fact]
+        public void TestInput_Parses()
+        {
+            var expected = new List<(int, int)> { (1, 2), (2, 1), (3, 3) };
+
+            var path = "Day 3\\TestInput.txt";
+            var parsed = _parser.Parse(path);
+
+            Assert.Equal(expected, parsed);
+        }
+
+        [Fact]
+        public void Part1_Test()
+        {
+            var expected = 15;
+
+            var path = "Day 3\\TestInput.txt";
+            var data = _parser.Parse(path);
+
+            var result = _solver.SolvePart1(data);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part1_Actual()
+        {
+            // too low
+            var expected = 12940;
+
+            var path = "Day 3\\Input.txt";
+            var data = _parser.Parse(path);
+
+            var result = _solver.SolvePart1(data);
+
+            Assert.Equal(expected, result);
+        }
+
+        //[Fact]
+        //public void Part1_MostCalories_Actual()
+        //{
+        //    var expected = 69693;
+
+        //    var path = "Day 3\\Input.txt";
+        //    var data = _parser.Parse(path);
+        //    var mostCalories = _solver.SolvePart1(data);
+
+        //    Assert.Equal(expected, mostCalories);
+        //}
+
+        //[Fact]
+        //public void Part2_Top3_Test()
+        //{
+        //    var expected = 45000;
+
+        //    var path = "Day 1\\D1TestInput.txt";
+        //    var data = _parser.Parse(path);
+        //    var mostCalories = _solver.SolvePart2(data);
+
+        //    Assert.Equal(expected, mostCalories);
+        //}
+
+        //[Fact]
+        //public void Part2_Top3_Actual()
+        //{
+        //    var expected = 200945;
+
+        //    var path = "Day 1\\D1Input.txt";
+        //    var data = _parser.Parse(path);
+        //    var mostCalories = _solver.SolvePart2(data);
+
+        //    Assert.Equal(expected, mostCalories);
+        //}
+    }
+}
