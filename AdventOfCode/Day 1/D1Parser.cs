228c0d629ef82edc6f00d8fe847d571db87913e5
Meriem Chelbi <meriem.chelbi@thetrainline.com>
2022-12-05 11:46:49 +0000

    D1 parse input

---
+++ AdventOfCode/Day 1/D1Parser.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode.Day_1
+{
+    public class D1Parser
+    {
+        public List<int> Parse(string inputPath)
+        {
+            var output = new List<int>();
+
+            var absolutePath = Path.GetFullPath(inputPath);
+
+            using (var sr = new StreamReader(absolutePath))
+            {
+                string line;
+
+                var elfCalories = new List<int>();
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (line.Length != 0)
+                    {
+                        var number = int.Parse(line);
+                        elfCalories.Add(number);
+                    }
+                    else
+                    {
+                        GetTotalCalories(output, elfCalories);
+                    }
+                }
+                GetTotalCalories(output, elfCalories);
+            }
+
+            return output;
+        }
+
+        private static void GetTotalCalories(List<int> output, List<int> elfCalories)
+        {
+            var totalCalories = 0;
+            elfCalories.ForEach(c => totalCalories += c);
+            output.Add(totalCalories);
+            elfCalories.Clear();
+        }
+    }
+}
---
+++ AdventOfCode/Day 1/D1Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AdventOfCode.Day_1
+{
+    public class D1Tests
+    {
+        private readonly D1Parser _parser;
+        private readonly D1Solver _solver;
+
+        public D1Tests()
+        {
+            _parser = new D1Parser();
+            _solver = new D1Solver();
+        }
+
+        [Fact]
+        public void TestInput_Parses_AsListOfInt()
+        {
+            var expected = new List<int> { 6000, 4000, 11000, 24000, 10000 };
+
+            var path = "Day 1\\D1TestInput.txt";
+            var parsed = _parser.Parse(path);
+
+            Assert.Equal(expected, parsed);
+        }
+    }
+}
