ba50acd4c2af57e48bf9452770b2a06b99f8f34a
Meriem Chelbi <meriem.chelbi@thetrainline.com>
2022-12-11 11:32:33 +0000

    Create ToListOfStringParser.cs

---
+++ AdventOfCode/Parsers/ToListOfStringParser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode.Parsers
+{
+    public class ToListOfStringParser
+    {
+        public List<string> Parse(string inputPath)
+        {
+            var output = new List<string>();
+
+            var absolutePath = Path.GetFullPath(inputPath);
+
+            using (var sr = new StreamReader(absolutePath))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    output.Add(line);
+                }
+            }
+
+            return output;
+        }
+    }
+}
