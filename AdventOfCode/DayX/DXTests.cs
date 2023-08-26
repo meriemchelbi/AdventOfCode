cfa57e0e33ffd5471e00b759654f7eb25c9ea89b
sprogslegs <47434119+sprogslegs@users.noreply.github.com>
2023-08-26 15:34:26 +0100

    Add template day

---
+++ AdventOfCode/DayX/DXTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Day_X
+{
+    public class DXTests
+    {
+        private readonly DXParser _parser;
+        private readonly DXSolver _solver;
+
+        public DXTests()
+        {
+            _parser = new DXParser();
+            _solver = new DXSolver();
+        }
+
+        [Fact]
+        public void TestInput_Parses_AsListOfInt()
+        {
+            var expected = new List<int> { 0 };
+
+            var path = "DayX\\DXTestInput.txt";
+            var parsed = _parser.Parse(path);
+
+            Assert.Equal(expected, parsed);
+        }
+        
+        [Fact]
+        public void Part1_Test()
+        {
+            var expected = 24000;
+
+            var path = "DayX\\DXTestInput.txt";
+            var data = _parser.Parse(path);
+            var mostCalories = _solver.SolvePart1(data);
+
+            Assert.Equal(expected, mostCalories);
+        }
+
+        [Fact]
+        public void Part1_Actual()
+        {
+            var expected = 69693;
+
+            var path = "DayX\\DXInput.txt";
+            var data = _parser.Parse(path);
+            var mostCalories = _solver.SolvePart1(data);
+
+            Assert.Equal(expected, mostCalories);
+        }
+
+        [Fact]
+        public void Part2_Test()
+        {
+            var expected = 45000;
+
+            var path = "DayX\\DXTestInput.txt";
+            var data = _parser.Parse(path);
+            var mostCalories = _solver.SolvePart2(data);
+
+            Assert.Equal(expected, mostCalories);
+        }
+        
+        [Fact]
+        public void Part2_Actual()
+        {
+            var expected = 200945;
+
+            var path = "DayX\\DXInput.txt";
+            var data = _parser.Parse(path);
+            var mostCalories = _solver.SolvePart2(data);
+
+            Assert.Equal(expected, mostCalories);
+        }
+    }
+}
