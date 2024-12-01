79b25d282116be501f0f0918c172eca140f219a3
sprogslegs <meriem.chelbi@gmail.com>
2024-12-01 08:32:15 +0000

    Day 2 setup

---
+++ AdventOfCode/Day2/D2Solver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Day2
+{
+    public class D2Solver
+    {
+        public int SolvePart1(List<int> input)
+        {
+            return 0;
+        }
+
+        public int SolvePart2(List<int> input)
+        {
+            return 0;
+        }
+    }
+}
