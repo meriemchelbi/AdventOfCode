2bbc9681fbcea6ba80a8a4129edb10111ce5665f
sprogslegs <meriem.chelbi@gmail.com>
2024-12-07 07:24:28 +0000

    Day 7 setup

---
+++ AdventOfCode/Day7/D7Solver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Day7
+{
+    public class D7Solver
+    {
+        public int SolvePart1(List<int> input)
+        {
+            return 0;
+        }
+
+        public int SolvePart2(List<int> input)
+        {
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode/Day7/D7Tests.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode.Day7
+{
+    public class D7Tests
+    {
+        private readonly D7Parser _parser;
+        private readonly D7Solver _solver;
+
+        public D7Tests()
+        {
+            _parser = new D7Parser();
+            _solver = new D7Solver();
+        }
+
+        [Fact]
+        public void TestInput_Parses_AsListOfInt()
+        {
+            var expected = new List<int> { 0 };
+
+            var path = "Day7\\D7TestInput.txt";
+            var parsed = _parser.Parse(path);
+
+            Assert.Equal(expected, parsed);
+        }
+
+        [Fact]
+        public void Part1_Test()
+        {
+            var expected = 24000;
+
+            var path = "Day7\\D7TestInput.txt";
+            var data = _parser.Parse(path);
+            var result = _solver.SolvePart1(data);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part1_Actual()
+        {
+            var expected = 69693;
+
+            var path = "Day7\\D7Input.txt";
+            var data = _parser.Parse(path);
+            var result = _solver.SolvePart1(data);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_Test()
+        {
+            var expected = 45000;
+
+            var path = "Day7\\D7TestInput.txt";
+            var data = _parser.Parse(path);
+            var result = _solver.SolvePart2(data);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_Actual()
+        {
+            var expected = 200945;
+
+            var path = "Day7\\D7Input.txt";
+            var data = _parser.Parse(path);
+            var result = _solver.SolvePart2(data);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
